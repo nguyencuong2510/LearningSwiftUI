25ecf718f570ee3e4c8457d2a491f49d5e422161
nguyen cuong <vancuong.nguyen@savvycomsoftware.com>
2025-08-18 00:30:34 +0700

    update mask layer in text

---
+++ LearningSwiftUI/ColorManager/ColorManager.swift
@@ -0,0 +1,18 @@
+//
+//  ColorManager.swift
+//  LearningSwiftUI
+//
+//  Created by nguyen cuong on 18/8/25.
+//
+
+import SwiftUI
+
+struct ColorManager: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ColorManager()
+}
